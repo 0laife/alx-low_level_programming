d404ded55a6e392d2db3b6b09f967ca0e0eeb2cb
0laife <olaife11111@gmail.com>
2023-03-22 03:27:02 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,14 +12,14 @@ int num, mult, prod;
 
 if (n >= 0 && n <= 15)
 {
-for (num = 0 ; num <= n ; num++)
+for (num = 0; num <= n; num++)
 {
 _putchar('0');
-for (mult = 1 ; mult <= n ; mult++)
+for (mult = 1; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
-prod = num + mult;
+prod = num * mult;
 if (prod <= 99)
 _putchar(' ');
 if (prod <= 9)
