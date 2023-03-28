f4c2194c9ac7db3a13679de94741591b3c9c4715
0laife <olaife11111@gmail.com>
2023-03-27 17:47:10 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+
+                                                                               {
+                                password[index] -= diff_half1;                                                                                                                                                                                                                                                                  break;                                                                                                                                                                                                                                                                                  }
+
+                                                                       }
+
+                                                                       for (index = 0; password[index]; index++)
+
+                                                                       {
+
+                                                                               if (password[index] >= (33 + diff_half2))                                                                                                                                                                                                                                                       {
+
+                                                                                       password[index] -= diff_half2;
+
+                                                                                       break;
+
+                                                                               }
+
+                                                                       }
+        }
+
+        printf("%s", password);
+        return (0);
+}
