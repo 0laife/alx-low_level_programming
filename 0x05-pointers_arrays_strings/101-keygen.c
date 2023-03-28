864cbc6a8c782a2eb557dd31f26898329c2a7348
0laife <olaife11111@gmail.com>
2023-03-27 18:12:04 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,23 @@
+(
+{
+password(index) -= diff_half1;                                                                                                                                                                                                                  break;
 
-                                                                               {
-                                password[index] -= diff_half1;                                                                                                                                                                                                                                                                  break;                                                                                                                                                                                                                                                                                  }
+{
+for (index = 0; password(index); index++)
 
-                                                                       }
+{
+if (password(index) >= (33 + diff_half2))                                                                                                                                                                                                       {
 
-                                                                       for (index = 0; password[index]; index++)
+                                                                                password(index) -= diff_half2;
 
-                                                                       {
+                                                                                break;
 
-                                                                               if (password[index] >= (33 + diff_half2))                                                                                                                                                                                                                                                       {
-
-                                                                                       password[index] -= diff_half2;
-
-                                                                                       break;
-
-                                                                               }
-
-                                                                       }
-        }
-
-        printf("%s", password);
-        return (0);
 }
+
+}                                                                       
+}
+
+printf("%s", password);
+return (0);
+}
+)
